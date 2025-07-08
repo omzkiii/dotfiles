eddbe2648322d6d9e4e0c7193c712e3bac87cde6
omzkiii <geogeomarsantos@gmail.com>
2025-07-08 18:18:37 +0800

    add vue lsp in nvim

--- kickstart/.config/nvim/lsp/vue.lua
+++ kickstart/.config/nvim/lsp/vue.lua
@@ -1,4 +1,31 @@
 return {
   cmd = { "vue-language-server", "--stdio" },
   filetypes = { "vue" },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
+  settings = {
+    typescript = {
+      inlayHints = {
+        enumMemberValues = {
+          enabled = true,
+        },
+        functionLikeReturnTypes = {
+          enabled = true,
+        },
+        propertyDeclarationTypes = {
+          enabled = true,
+        },
+        parameterTypes = {
+          enabled = true,
+          suppressWhenArgumentMatchesName = true,
+        },
+        variableTypes = {
+          enabled = true,
+        },
+      },
+    },
+  },
 }
