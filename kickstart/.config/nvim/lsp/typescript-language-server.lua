50f3547bbc0bac02200d780a08745f82d014a6e8
omzkiii <geogeomarsantos@gmail.com>
2025-07-08 18:18:23 +0800

    update tsls config in nvim

--- kickstart/.config/nvim/lsp/typescript-language-server.lua
+++ kickstart/.config/nvim/lsp/typescript-language-server.lua
@@ -1,4 +1,30 @@
 return {
   cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+        languages = { "vue" },
+      },
+    },
+  },
+  settings = {
+    typescript = {
+      tsserver = {
+        useSyntaxServer = false,
+      },
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
 }
