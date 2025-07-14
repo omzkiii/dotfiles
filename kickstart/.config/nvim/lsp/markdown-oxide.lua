0d21995141b435d4e2871f35dad54456a701577b
omzkiii <geogeomarsantos@gmail.com>
2025-07-15 01:58:36 +0800

    add markdown-oxide lsp

---
+++ kickstart/.config/nvim/lsp/markdown-oxide.lua
@@ -0,0 +1,21 @@
+return {
+  filetypes = { "markdown" },
+  cmd = { "markdown-oxide" },
+  on_attach = function(_, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, "LspToday", function()
+      vim.lsp.buf.execute_command { command = "jump", arguments = { "today" } }
+    end, {
+      desc = "Open today's daily note",
+    })
+    vim.api.nvim_buf_create_user_command(bufnr, "LspTomorrow", function()
+      vim.lsp.buf.execute_command { command = "jump", arguments = { "tomorrow" } }
+    end, {
+      desc = "Open tomorrow's daily note",
+    })
+    vim.api.nvim_buf_create_user_command(bufnr, "LspYesterday", function()
+      vim.lsp.buf.execute_command { command = "jump", arguments = { "yesterday" } }
+    end, {
+      desc = "Open yesterday's daily note",
+    })
+  end,
+}
