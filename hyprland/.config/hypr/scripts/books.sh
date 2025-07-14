02cac09459b3808412188e72d436f624f17e8492
omzkiii <geogeomarsantos@gmail.com>
2025-07-15 01:56:00 +0800

    update books script fzf

--- hyprland/.config/hypr/scripts/books.sh
+++ hyprland/.config/hypr/scripts/books.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # source ~/.zshrc
 book=$(
-	find -P ~/Documents/Books/ -type f | fzf --delimiter / --with-nth -1 --layout=reverse --border=sharp --border-label="󱉟 Books" --pointer=󰄾 --color=16 --color=border:cyan --color=current-bg:bright-black --prompt=" │ " --bind="j:down" --bind="k:up" --bind="i:enable-search+unbind(j)+unbind(k)+unbind(i)" --bind="esc:disable-search+rebind(j)+rebind(k)+rebind(i)"
+	find -P ~/Documents/Books/ -type f | fzf --delimiter / --with-nth -1 --layout=reverse --border=sharp --border-label="󱉟 Books" --pointer=󰄾 --color=16 --color=border:cyan --color=current-bg:bright-black --prompt=" │ " --bind="j:down" --bind="k:up" --bind="q:abort" --bind="i:enable-search+unbind(j)+unbind(k)+unbind(i)+unbind(q)" --bind="esc:disable-search+rebind(j)+rebind(k)+rebind(i)+rebind(q)"
 )
 
 if [[ -n "$book" ]]; then
