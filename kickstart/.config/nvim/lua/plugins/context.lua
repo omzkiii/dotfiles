4637f1312cbb9d0ca53809861ecf1bb473470e78
omzkiii <geogeomarsantos@gmail.com>
2025-07-16 00:06:32 +0800

    bring back context in nvim

--- kickstart/.config/nvim/lua/plugins/context.lua
+++ kickstart/.config/nvim/lua/plugins/context.lua
@@ -11,7 +11,7 @@ return {
       line_numbers = true,
       multiline_threshold = 20, -- Maximum number of lines to show for a single context
       trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-      mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
+      mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
       -- Separator between context and content. Should be a single character string, like '-'.
       -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
       -- separator = "─",
@@ -22,14 +22,13 @@ return {
       require("treesitter-context").setup(opts)
 
       local colors = function()
-        local ul = vim.api.nvim_get_hl(0, { name = "IblScope", link = false })
-        -- vim.api.nvim_set_hl(0, "LeapLabel", { bg = yellow.fg, fg = norm.bg, bold = true })
-        -- vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+        local ul = vim.api.nvim_get_hl(0, { name = "SnacksIndentScope", link = false })
+        local ul = vim.api.nvim_get_hl(0, { name = "Comment", link = false })
 
         vim.api.nvim_set_hl(0, "TreesitterContextBottom", { fg = "NONE", bg = "NONE", underline = true, sp = ul.fg })
         vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { link = "TreesitterContextBottom" })
         vim.api.nvim_set_hl(0, "TreesitterContext", { link = "Normal" }) -- or LineNr, Normal, etc.
-        vim.api.nvim_set_hl(0, "TreesitterContextSeparator", { link = "IblScope" }) -- or LineNr, Normal, etc.
+        vim.api.nvim_set_hl(0, "TreesitterContextSeparator", { link = "SnacksIndentScope" }) -- or LineNr, Normal, etc.
         vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { link = "LineNr" })
       end
       colors()
