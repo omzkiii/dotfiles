038844194cca5be733e367153a5a59aa3665808f
omzkiii <geogeomarsantos@gmail.com>
2025-07-22 23:05:38 +0800

    nvim: add dracula to init

--- kickstart/.config/nvim/init.lua
+++ kickstart/.config/nvim/init.lua
@@ -187,6 +187,7 @@ require("lazy").setup({
 
 -- vim.cmd.colorscheme "nvchad"
 -- vim.cmd.colorscheme "ayu-dark"
+-- vim.cmd.colorscheme "dracula"
 vim.cmd.colorscheme "kanagawa"
 -- vim.cmd.colorscheme "solarized"
 -- vim.cmd.colorscheme "gruvbox"
