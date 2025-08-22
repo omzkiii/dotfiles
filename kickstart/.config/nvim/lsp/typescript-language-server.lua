fc2cddfb60e4145ff51ac1c9a66e6a59f6f49ab2
omzkiii <geogeomarsantos@gmail.com>
2025-08-22 23:33:55 +0800

    nvim: update ts lsp

--- kickstart/.config/nvim/lsp/typescript-language-server.lua
+++ kickstart/.config/nvim/lsp/typescript-language-server.lua
@@ -10,6 +10,89 @@ return {
       },
     },
   },
+  root_dir = function(bufnr, on_dir)
+    -- The project root is where the LSP can be started from
+    -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+    -- We select then from the project root, which is identified by the presence of a package
+    -- manager lock file.
+    local project_root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb" }
+    local project_root = vim.fs.root(bufnr, project_root_markers)
+    if not project_root then
+      return nil
+    end
+
+    -- We know that the buffer is using Typescript if it has a config file
+    -- in its directory tree.
+    local ts_config_files = { "tsconfig.json", "jsconfig.json" }
+    local is_buffer_using_typescript = vim.fs.find(ts_config_files, {
+      path = vim.api.nvim_buf_get_name(bufnr),
+      type = "file",
+      limit = 1,
+      upward = true,
+      stop = vim.fs.dirname(project_root),
+    })[1]
+    if not is_buffer_using_typescript then
+      return nil
+    end
+
+    on_dir(project_root)
+  end,
+  handlers = {
+    -- handle rename request for certain code actions like extracting functions / types
+    ["_typescript.rename"] = function(_, result, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      vim.lsp.util.show_document({
+        uri = result.textDocument.uri,
+        range = {
+          start = result.position,
+          ["end"] = result.position,
+        },
+      }, client.offset_encoding)
+      vim.lsp.buf.rename()
+      return vim.NIL
+    end,
+  },
+  commands = {
+    ["editor.action.showReferences"] = function(command, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      local file_uri, position, references = unpack(command.arguments)
+
+      local quickfix_items = vim.lsp.util.locations_to_items(references, client.offset_encoding)
+      vim.fn.setqflist({}, " ", {
+        title = command.title,
+        items = quickfix_items,
+        context = {
+          command = command,
+          bufnr = ctx.bufnr,
+        },
+      })
+
+      vim.lsp.util.show_document({
+        uri = file_uri,
+        range = {
+          start = position,
+          ["end"] = position,
+        },
+      }, client.offset_encoding)
+
+      vim.cmd "botright copen"
+    end,
+  },
+  on_attach = function(client, bufnr)
+    -- ts_ls provides `source.*` code actions that apply to the whole file. These only appear in
+    -- `vim.lsp.buf.code_action()` if specified in `context.only`.
+    vim.api.nvim_buf_create_user_command(bufnr, "LspTypescriptSourceAction", function()
+      local source_actions = vim.tbl_filter(function(action)
+        return vim.startswith(action, "source.")
+      end, client.server_capabilities.codeActionProvider.codeActionKinds)
+
+      vim.lsp.buf.code_action {
+        context = {
+          only = source_actions,
+        },
+      }
+    end, {})
+  end,
   settings = {
     typescript = {
       tsserver = {
