08ab1bb310c84530165987d77088134fe261c86c
omzkiii <geogeomarsantos@gmail.com>
2025-08-22 23:31:53 +0800

    hyrpland: small script change

--- hyprland/.config/hypr/scripts/ff.sh
+++ hyprland/.config/hypr/scripts/ff.sh
@@ -7,6 +7,7 @@ HISTORY_FILE="$HOME/.fzf_project_history"
 dir=$(
   (
     cat "$HISTORY_FILE"
+    # fd -t d --base-directory ~/Documents/Projects/
     find -P ~/Documents/Projects/ -type d
   ) | fzf --preview="tree -C -C {}" --delimiter / --with-nth 6.. --layout=default --border=sharp --preview-border=sharp --border-label=" Projects" --pointer=󰄾 --color=16 --color=border:cyan --color=current-bg:bright-black --prompt=" │" --bind="j:down" --bind="k:up" --bind="q:abort" --bind="i:enable-search+unbind(j)+unbind(k)+unbind(i)+unbind(q)" --bind="esc:disable-search+rebind(j)+rebind(k)+rebind(i)+rebind(q)"
 )
